96f18db15065d55211b8cd566ea9898dd6eefdc9
POULARD Thomas <pro.thomaspoulard@gmail.com>
2025-11-02 20:18:17 +0100

    fix: run yarn prettier --write

--- src/app/kick-boxing/page.tsx
+++ src/app/kick-boxing/page.tsx
@@ -5,7 +5,11 @@ export default function Coaching() {
   return (
     <section className="flex flex-col gap-12 py-12 lg:pt-12 lg:pb-24">
       <ActivitySection activity="kick-boxing" hasImage={false} hasBoldTitles />
-      <LandscapeImagesGalery subDirectory="coaching" slug="kick-boxing" title="Activités de groupe - Cours privés" />
+      <LandscapeImagesGalery
+        subDirectory="coaching"
+        slug="kick-boxing"
+        title="Activités de groupe - Cours privés"
+      />
     </section>
   )
 }
