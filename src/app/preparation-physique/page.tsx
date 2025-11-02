405ee7c028c9dbb854e20d7bc89eedcb2ca510bf
POULARD Thomas <pro.thomaspoulard@gmail.com>
2025-11-02 19:40:07 +0100

    feat: page "preparation physique"

--- src/app/preparation-physique/page.tsx
+++ src/app/preparation-physique/page.tsx
@@ -1,9 +1,13 @@
 import ActivitySection from '@/components/sections/ActivitySection'
+import PortraitImagesGalery from '@/components/sections/PortraitImagesGalery'
 
-export default function Nutrition() {
+export default function PreparationPhysique() {
   return (
-    <section className="flex flex-col gap-12 pt-4 pb-12 lg:gap-24 lg:pt-12 lg:pb-24">
-      <ActivitySection activity="preparation-physique" hasBoldTitles />
+    <section className="flex flex-col gap-12 pt-8 lg:gap-24 lg:pt-16">
+      <h1>Préparation physique</h1>
+      <PortraitImagesGalery subDirectory="coaching" slug="preparation-physique" classNames="sm:-mt-8 md:-mt-12" />
+      <ActivitySection activity="preparation-physique" hasImage={false} />
+      <ActivitySection activity="musculation" hasImage={false} />
     </section>
   )
 }
--- src/components/coaching/HomeCoachingCard.tsx
+++ src/components/coaching/HomeCoachingCard.tsx
@@ -21,7 +21,7 @@ const HomeCoachingCard: React.FC<IProps> = ({ isReversed = false, coachingSectio
             return (
               <div
                 key={i}
-                className="flex flex-col justify-center sm:gap-4 text-justify md:items-start"
+                className="flex flex-col justify-center gap-3 sm:gap-8 text-justify md:items-start"
               >
                 {hasBoldTitles ?
                   <h1>{section.title}</h1>
@@ -33,9 +33,24 @@ const HomeCoachingCard: React.FC<IProps> = ({ isReversed = false, coachingSectio
                   {section.shortDescription && <p className="mb-2">{section.shortDescription}</p>}
                   <p>{section.description}</p>
                 </div>
+                {
+                  section?.items &&
+                  <ul className='pl-5 list-disc font-light'>
+                    {
+                      section?.items?.map((el, i) => {
+                        return <li>{el}</li>
+                      })
+                    }
+                  </ul>
+                }
+                {
+                  section.conclusion && <p>{section.conclusion}</p>
+
+                }
               </div>
             )
-          })}
+          })
+        }
       </div>
       {
         hasImage && <img
