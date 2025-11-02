ef49aa5553522d2a4b641ca2f7abe5f246d8662b
POULARD Thomas <pro.thomaspoulard@gmail.com>
2025-11-02 11:51:44 +0100

    feat: handle format of 3 or 4 images

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -3,9 +3,7 @@ import Image, { ImageProps } from 'next/image'
 const basePath = process.env.BASE_PATH;
 
 const ImageCard = ({ src, ...rest }: ImageProps) => (
-  <div>
-    <Image src={`${basePath || ''}${src}`} {...rest} />
-  </div>
+  <Image src={`${basePath || ''}${src}`} width={0} height={0} sizes="500px" className='h-full w-full object-cover sm:max-h-[660px]' {...rest} />
 )
 
 export default ImageCard
--- src/components/sections/HomepageCardsSection.tsx
+++ src/components/sections/HomepageCardsSection.tsx
@@ -1,16 +1,27 @@
+'use client'
 import { introductionContent } from '@/data/content'
+import { useRouter } from 'next/navigation'
 import ImageCard from '../ImageCard';
 
 const HomepageCardsSection = () => {
   const imgDirectoryName = '/static/images/coaching/homepage/';
+  const activities = introductionContent.activities;
+  const router = useRouter()
 
   return (
-    <section className="-mt-4 flex flex-col gap-8 sm:-mt-0 lg:gap-20">
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-2 sm:gap-6">
-        {
-          introductionContent.activities.map((activity, i) => {
-            return <ImageCard key={i} src={imgDirectoryName + activity.imgName} alt={activity.imgAlt} width={0} height={0} sizes="500px" className='h-full w-full object-cover sm:max-h-[660px]' />
-          })
+    <section className="-mt-4 flex flex-col justify-center gap-8 sm:-mt-0 lg:gap-20">
+      <div
+        className={
+          activities.length <= 3
+            ? "grid grid-cols-1 sm:grid-cols-3 gap-2 sm:gap-6"
+            : "grid grid-cols-2 sm:grid-cols-4 gap-2 sm:gap-6"
+        }
+      >
+        {introductionContent.activities.map((activity, i) => {
+          return <button key={i} onClick={() => router.push(activity.path)} className="cursor-pointer">
+            <ImageCard src={imgDirectoryName + activity.imgName} alt={activity.imgAlt} />
+          </button>
+        })
         }
       </div >
     </section >
